4b5ba44e8b4b73c5ad11ac004699f3fc6acb2afb
sanjukumar1 <sarojkumar.bit@gmail.com>
2022-04-14 20:14:30 +0530

    commit 1

---
+++ PLOT1.R
@@ -0,0 +1,10 @@
+power_data <- read.table("household_power_consumption.txt",skip=1,sep=";")
+head(power_data)
+names(power_data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power_data,power_data$Date=="1/2/2007" | power_data$Date =="2/2/2007")
+head(subpower)
+png(file="plot1.png")
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+title(main="Global Active Power")
+dev.off()
+
